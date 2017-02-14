497ce9cc03f073eaf10972a4a108cc470f33e430
wwagner4 <wwagner4@gmail.com>
2017-02-14 18:51:30 +0100

    added some concept for creating sensors from server and player

--- machinelearning-2017/playerpos/src/main/scala/playerpos/Main.scala
+++ machinelearning-2017/playerpos/src/main/scala/playerpos/Main.scala
@@ -1,7 +1,57 @@
 package playerpos
 
+import vsoc.behaviour.Sensors
+import vsoc.server.Server
+
 object Main extends App {
+
+  val f = new VsocFactory()
   
-  println("hallo w")
+  val s = f.server
+  val p = f.player
+  
+  val pos = Pos(10, 20, 222)
+  
+  s.setPlayer(p, pos)
+  s.takeOneStep()
+  
+  val sensors = p.sensors
+  
+  
+  
+}
+
+class VsocFactory {
+  
+  def server: Server = new Server {
+    
+    val s = new vsoc.server.Server()
+    
+    def setPlayer(p: Player,pas: Pos): Unit = ??? 
+    
+    def takeOneStep(): Unit = ???
+    
+  }
+  
+  def player: Player = new Player {
+      def sensors: Sensors = ???
+  }
+  
+}
+
+case class Pos(x: Double, y: Double, dir: Double)
+
+
+trait Server {
+
+  def takeOneStep()
+  
+  def setPlayer(p: Player, pas: Pos)
+  
+}
+
+trait Player {
+  
+  def sensors: Sensors
   
 }
