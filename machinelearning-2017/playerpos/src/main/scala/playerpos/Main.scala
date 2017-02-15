6109d27317947997b4b6c48fee9f23b9f6296729
wwagner4 <wwagner4@gmail.com>
2017-02-15 07:48:05 +0100

    added camp with all necessary classes

--- machinelearning-2017/playerpos/src/main/scala/playerpos/Main.scala
+++ machinelearning-2017/playerpos/src/main/scala/playerpos/Main.scala
@@ -1,57 +1,34 @@
 package playerpos
 
-import vsoc.behaviour.Sensors
-import vsoc.server.Server
+import vsoc.camps.VectorFunction
+import vsoc.camps.AbstractCamp
+import vsoc.camps.Member
+import vsoc.camps.VectorFunctionBehaviour
+import vsoc.camps.SensorsToVector
+import vsoc.camps.VectorFunctionBehaviourController
+import vsoc.server.VsocPlayer
 
 object Main extends App {
 
-  val f = new VsocFactory()
-  
-  val s = f.server
-  val p = f.player
-  
-  val pos = Pos(10, 20, 222)
-  
-  s.setPlayer(p, pos)
-  s.takeOneStep()
-  
-  val sensors = p.sensors
-  
-  
-  
 }
 
-class VsocFactory {
-  
-  def server: Server = new Server {
-    
-    val s = new vsoc.server.Server()
-    
-    def setPlayer(p: Player,pas: Pos): Unit = ??? 
-    
-    def takeOneStep(): Unit = ???
-    
-  }
-  
-  def player: Player = new Player {
-      def sensors: Sensors = ???
-  }
-  
+class PlayerposCamp extends AbstractCamp[Member[VectorFunctionBehaviourController[PlayerposVectorFunction]], PlayerposVectorFunction] {
+  def createNextGeneration(): Unit = ???
+  def eastPlayerCount(): Int = ???
+  def getMembers(): java.util.List[vsoc.camps.Member[vsoc.camps.VectorFunctionBehaviourController[PlayerposVectorFunction]]] = ???
+  def initPlayersForMatch(): Unit = ???
+  def updateMemberFromPlayer(player: VsocPlayer, mem: Member[VectorFunctionBehaviourController[PlayerposVectorFunction]]): Unit = ???
+  def westPlayerCount(): Int = ???
+
 }
 
-case class Pos(x: Double, y: Double, dir: Double)
+class PlayerposBehaviour(vf: PlayerposVectorFunction, stv: SensorsToVector) extends VectorFunctionBehaviour[PlayerposVectorFunction](vf, stv) {
 
-
-trait Server {
-
-  def takeOneStep()
-  
-  def setPlayer(p: Player, pas: Pos)
-  
 }
 
-trait Player {
+class PlayerposVectorFunction extends VectorFunction {
   
-  def sensors: Sensors
-  
+  	def apply(in: Array[Double]): Array[Double] = ???
+
+
 }
