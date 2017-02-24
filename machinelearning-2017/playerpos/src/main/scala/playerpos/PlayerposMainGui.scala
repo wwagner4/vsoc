13e786ab8cd49cd6e4208aca4112f26f38652a42
wwagner4 <wwagner4@gmail.com>
2017-02-24 20:36:07 +0100

    playerpos gui ok

--- machinelearning-2017/playerpos/src/main/scala/playerpos/PlayerposMainGui.scala
+++ machinelearning-2017/playerpos/src/main/scala/playerpos/PlayerposMainGui.scala
@@ -23,20 +23,56 @@ import java.awt.event.ActionListener
 import java.awt.event.ActionEvent
 import java.awt.FlowLayout
 import vsoc.server.gui.FieldPanel
+import vsoc.server.gui.Simulation
+import vsoc.server.Server
+import scala.collection.JavaConversions._
+import atan.model.Controller
+import scala.util.Random
 
 object PlayerposMainGui extends App {
-  
+
   val f = new FieldFrame()
-  f.setSize(500, 500)
+  f.setSize(900, 600)
   f.setVisible(true)
 
 }
 
 class FieldFrame extends JFrame with WindowListener {
-  getContentPane.add(new FieldContentPanel())
   
+  val rand = new Random
+  
+  def createServer: Server = {
+    val s = ServerUtil.current().createServer(10, 10)
+    for (p <- s.getPlayers) {
+      p.setController(createController)
+    }
+    s
+  }
+
+  def ran(from: Int, to: Int): Int = {
+    require(from < to)
+    val w = to - from
+    val off = w / 2
+    rand.nextInt(w) - off
+  }
+  
+  def createController: Controller = {
+    val behav = new Behaviour() {
+      def apply(sens: Sensors, player: Player): Unit = {
+        println("" + sens)
+        player.move(ran(-60, 60), ran(-50, 50))
+        player.turn(ran(0, 360))
+      }
+      def getChild(): Optional[Behaviour] = Optional.empty()
+      def shouldBeApplied(sens: Sensors): Boolean = true
+    }
+    new BehaviourController(behav)
+  }
+
+  getContentPane.add(new FieldContentPanel(createServer))
+
   addWindowListener(this)
-  
+
   def windowActivated(evt: WindowEvent): Unit = ()
   def windowClosed(evt: WindowEvent): Unit = ()
   def windowClosing(evt: WindowEvent): Unit = System.exit(0)
@@ -44,29 +80,28 @@ class FieldFrame extends JFrame with WindowListener {
   def windowDeiconified(evt: WindowEvent): Unit = ()
   def windowIconified(evt: WindowEvent): Unit = ()
   def windowOpened(evt: WindowEvent): Unit = ()
-  
+
 }
 
-class FieldContentPanel extends JPanel with ActionListener {
-  
+class FieldContentPanel(sim: Server) extends JPanel with ActionListener {
+
   setLayout(new BorderLayout())
-  val fieldCanvas = new FieldPanel()
+  val field = new FieldPanel()
+  field.setSim(sim)
 
   val ctrlPanel = new JPanel()
   ctrlPanel.setLayout(new FlowLayout())
-  
+
   val takeStepButton = new JButton("take step")
   takeStepButton.addActionListener(this)
-  
+
   ctrlPanel.add(takeStepButton)
-  
-  add(fieldCanvas, BorderLayout.CENTER)
+
+  add(field, BorderLayout.CENTER)
   add(ctrlPanel, BorderLayout.SOUTH)
-  
+
   def actionPerformed(evt: ActionEvent): Unit = {
-    println("action")
+    sim.takeStep()
   }
-  
-  
-  
+
 }
--- vsoc-2007/vsoc-core/src/main/java/vsoc/server/gui/VsocTestFrame.java
+++ vsoc-2007/vsoc-core/src/main/java/vsoc/server/gui/VsocTestFrame.java
@@ -1,175 +1,168 @@
 package vsoc.server.gui;
 
-import atan.model.Controller;
-import atan.model.Player;
-import java.util.Optional;
-import java.util.Random;
+import java.util.*;
 import java.util.concurrent.ExecutorService;
-import java.util.logging.Level;
-import java.util.logging.Logger;
-import javax.swing.SwingUtilities;
-import vsoc.behaviour.Behaviour;
-import vsoc.behaviour.BehaviourController;
-import vsoc.behaviour.Sensors;
-import vsoc.server.Server;
-import vsoc.server.ServerUtil;
+
+import atan.model.*;
+import vsoc.behaviour.*;
+import vsoc.server.*;
 
 public class VsocTestFrame extends javax.swing.JFrame {
 
-    private ExecutorService es = java.util.concurrent.Executors.newFixedThreadPool(10);
+	private static final long serialVersionUID = 1L;
 
-    private Random ran = new Random();
+	private ExecutorService es = java.util.concurrent.Executors.newFixedThreadPool(10);
 
-    Optional<Server> srv = Optional.empty();
+	private Random ran = new Random();
 
-    /**
-     * Creates new form VsocTestFrame
-     */
-    public VsocTestFrame() {
-        initComponents();
-        srv = Optional.of(ServerUtil.current().createServer(2, 2));
-        srv.ifPresent(s -> {
-            s.getPlayers().stream().forEach((p) -> {
-                p.setController(createController());
-            });
-            this.fieldPanel.setSim(s);
-        });
-    }
+	Optional<Server> srv = Optional.empty();
 
-    @SuppressWarnings("unchecked")
-    private void initComponents() {
+	/**
+	 * Creates new form VsocTestFrame
+	 */
+	public VsocTestFrame() {
+		initComponents();
+		srv = Optional.of(ServerUtil.current().createServer(1, 0));
+		srv.ifPresent(s -> {
+			s.getPlayers().stream().forEach((p) -> {
+			  p.setController(createController());
+		  });
+			this.fieldPanel.setSim(s);
+		});
+	}
 
-        contPanel = new javax.swing.JPanel();
-        cntrPanel = new javax.swing.JPanel();
-        takeStep = new javax.swing.JButton();
-        takeSteps = new javax.swing.JButton();
-        fieldPanel = new FieldPanel();
+	private void initComponents() {
 
-        setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
-        setTitle("vsoc test");
-        setName("vsocTestFrame"); // NOI18N
+		contPanel = new javax.swing.JPanel();
+		cntrPanel = new javax.swing.JPanel();
+		takeStep = new javax.swing.JButton();
+		takeSteps = new javax.swing.JButton();
+		fieldPanel = new FieldPanel();
 
-        contPanel.setLayout(new java.awt.BorderLayout());
+		setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
+		setTitle("vsoc test");
+		setName("vsocTestFrame"); // NOI18N
 
-        takeStep.setText("take step");
-        takeStep.addActionListener(new java.awt.event.ActionListener() {
-            public void actionPerformed(java.awt.event.ActionEvent evt) {
-                takeStepActionPerformed(evt);
-            }
-        });
-        takeSteps.setText("take steps");
-        takeSteps.addActionListener(new java.awt.event.ActionListener() {
-            public void actionPerformed(java.awt.event.ActionEvent evt) {
-                takeStepsActionPerformed(evt);
-            }
-        });
-        cntrPanel.add(takeStep);
-        cntrPanel.add(takeSteps);
+		contPanel.setLayout(new java.awt.BorderLayout());
 
-        contPanel.add(fieldPanel, java.awt.BorderLayout.CENTER);
-        contPanel.add(cntrPanel, java.awt.BorderLayout.SOUTH);
+		takeStep.setText("take step");
+		takeStep.addActionListener(new java.awt.event.ActionListener() {
+			public void actionPerformed(java.awt.event.ActionEvent evt) {
+				takeStepActionPerformed(evt);
+			}
+		});
+		takeSteps.setText("take steps");
+		takeSteps.addActionListener(new java.awt.event.ActionListener() {
+			public void actionPerformed(java.awt.event.ActionEvent evt) {
+				takeStepsActionPerformed(evt);
+			}
+		});
+		cntrPanel.add(takeStep);
+		cntrPanel.add(takeSteps);
 
-        javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
-        getContentPane().setLayout(layout);
-        layout.setHorizontalGroup(
-                layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-                .addComponent(contPanel, javax.swing.GroupLayout.DEFAULT_SIZE, 659, Short.MAX_VALUE)
-        );
-        layout.setVerticalGroup(
-                layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-                .addComponent(contPanel, javax.swing.GroupLayout.DEFAULT_SIZE, 445, Short.MAX_VALUE)
-        );
+		contPanel.add(fieldPanel, java.awt.BorderLayout.CENTER);
+		contPanel.add(cntrPanel, java.awt.BorderLayout.SOUTH);
 
-        pack();
-    }
+		javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
+		getContentPane().setLayout(layout);
+		layout.setHorizontalGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+		    .addComponent(contPanel, javax.swing.GroupLayout.DEFAULT_SIZE, 659, Short.MAX_VALUE));
+		layout.setVerticalGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+		    .addComponent(contPanel, javax.swing.GroupLayout.DEFAULT_SIZE, 445, Short.MAX_VALUE));
 
-    private void takeStepActionPerformed(java.awt.event.ActionEvent evt) {
-        System.out.println("takeStepActionPerformed");
-        srv.ifPresent(s -> {
-            es.execute(() -> {
-                s.takeStep();
-                System.out.println("take step");
-            });
-        });
-    }
+		pack();
+	}
 
-    private void takeStepsActionPerformed(java.awt.event.ActionEvent evt) {
-        System.out.println("takeStepsActionPerformed");
-        srv.ifPresent(s -> {
-            es.execute(() -> {
-                takeSteps(s);
-                System.out.println("take step");
-            });
-        });
-    }
+	private void takeStepActionPerformed(java.awt.event.ActionEvent evt) {
+		srv.ifPresent(s -> {
+			es.execute(() -> {
+			  s.takeStep();
+		  });
+		});
+	}
 
-    /**
-     * @param args the command line arguments
-     */
-    public static void main(String args[]) {
+	private void takeStepsActionPerformed(java.awt.event.ActionEvent evt) {
+		System.out.println("takeStepsActionPerformed");
+		srv.ifPresent(s -> {
+			es.execute(() -> {
+			  takeSteps(s);
+		  });
+		});
+	}
 
-        /* Create and display the form */
-        java.awt.EventQueue.invokeLater(new Runnable() {
-            public void run() {
-                new VsocTestFrame().setVisible(true);
-            }
-        });
-    }
+	/**
+	 * @param args
+	 *          the command line arguments
+	 */
+	public static void main(String args[]) {
 
-    private javax.swing.JPanel cntrPanel;
-    private javax.swing.JPanel contPanel;
-    private FieldPanel fieldPanel;
-    private javax.swing.JButton takeStep;
-    private javax.swing.JButton takeSteps;
+		/* Create and display the form */
+		java.awt.EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				new VsocTestFrame().setVisible(true);
+			}
+		});
+	}
 
-    private int ranw() {
-        return ran(-60, 60);
-    }
+	private javax.swing.JPanel cntrPanel;
+	private javax.swing.JPanel contPanel;
+	private FieldPanel fieldPanel;
+	private javax.swing.JButton takeStep;
+	private javax.swing.JButton takeSteps;
 
-    private int ranh() {
-        return ran(-50, 50);
-    }
+	private int ranw() {
+		return ran(-60, 60);
+	}
 
-    private Controller createController() {
-        Behaviour behav = new Behaviour() {
+	private int ranh() {
+		return ran(-50, 50);
+	}
+	private double ranTurn() {
+		return ran(0, 360);
+	}
 
-            @Override
-            public void apply(Sensors sens, Player player) {
-                player.move(ranw(), ranh());
-            }
 
-            @Override
-            public boolean shouldBeApplied(Sensors sens) {
-                return true;
-            }
+	private Controller createController() {
+		Behaviour behav = new Behaviour() {
+			private static final long serialVersionUID = 1L;
 
-            @Override
-            public Optional<Behaviour> getChild() {
-                return Optional.empty();
-            }
-        };
-        return new BehaviourController(behav);
-    }
+			@Override
+			public void apply(Sensors sens, Player player) {
+				player.move(ranw(), ranh());
+				player.turn(ranTurn());
+			}
 
-    private int ran(int from, int to) {
-        int w = to - from;
-        int off = w / 2;
-        return ran.nextInt(w) - off;
-    }
+			@Override
+			public boolean shouldBeApplied(Sensors sens) {
+				return true;
+			}
 
-    private void takeSteps(final Server s) {
-        for (int i = 0; i < 10; i++) {
-            s.takeStep();
-            pause(100);
-            System.out.println("take step " + i);
-        }
-    }
+			@Override
+			public Optional<Behaviour> getChild() {
+				return Optional.empty();
+			}
+		};
+		return new BehaviourController(behav);
+	}
 
-    private synchronized void pause(int ms) {
-        try {
-            wait(ms);
-        } catch (InterruptedException ex) {
-        }
-    }
+	private int ran(int from, int to) {
+		int w = to - from;
+		int off = w / 2;
+		return ran.nextInt(w) - off;
+	}
+
+	private void takeSteps(final Server s) {
+		for (int i = 0; i < 50; i++) {
+			s.takeStep();
+			pause(10);
+		}
+	}
+
+	private synchronized void pause(int ms) {
+		try {
+			wait(ms);
+		} catch (InterruptedException ex) {
+		}
+	}
 
 }
