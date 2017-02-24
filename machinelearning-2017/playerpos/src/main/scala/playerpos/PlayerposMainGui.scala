c1847dcf0e595bf8c679daef13b50cafe3e5b8fb
wwagner4 <wwagner4@gmail.com>
2017-02-24 23:15:53 +0100

    organised imports

--- machinelearning-2017/playerpos/src/main/scala/playerpos/PlayerposMainGui.scala
+++ machinelearning-2017/playerpos/src/main/scala/playerpos/PlayerposMainGui.scala
@@ -1,34 +1,30 @@
 package playerpos
 
-import java.awt.Color
 import atan.model.Player
-import java.util.Optional
+import atan.model.Controller
+
+import vsoc.server.Server
+import vsoc.server.VsocPlayer
+import vsoc.server.ServerUtil
+import vsoc.server.gui.FieldPanel
 import vsoc.behaviour.BehaviourController
 import vsoc.behaviour.Behaviour
+
 import vsoc.behaviour.Sensors
-import vsoc.server.ServerUtil
-import vsoc.server.gui.FieldCanvas
-import javax.swing.JComponent
-import javax.swing.JPanel
-import java.util.concurrent.Executors
-import scala.concurrent.Future
 import java.awt.BorderLayout
-import javax.swing.SwingUtilities
-import javax.swing.JFrame
+import java.awt.FlowLayout
 import java.awt.event.WindowListener
-import java.awt.event.WindowEvent
-import javax.swing.event._
-import javax.swing._
 import java.awt.event.ActionListener
 import java.awt.event.ActionEvent
-import java.awt.FlowLayout
-import vsoc.server.gui.FieldPanel
-import vsoc.server.gui.Simulation
-import vsoc.server.Server
-import scala.collection.JavaConversions._
-import atan.model.Controller
+import java.awt.event.WindowEvent
+import javax.swing.JFrame
+import javax.swing.JButton
+import javax.swing.JPanel
+import java.util.Optional
+
 import scala.util.Random
-import vsoc.server.VsocPlayer
+import scala.collection.JavaConversions._
+
 
 object PlayerposMainGui extends App {
 
@@ -39,9 +35,9 @@ object PlayerposMainGui extends App {
 }
 
 class FieldFrame extends JFrame with WindowListener {
-  
+
   val rand = new Random
-  
+
   def createServer: Server = {
     val s = ServerUtil.current().createServer(1, 0)
     for (p <- s.getPlayers) {
@@ -53,19 +49,23 @@ class FieldFrame extends JFrame with WindowListener {
   def ran(from: Int, to: Int): Int = {
     require(from < to)
     val w = to - from
-    val off = w / 2
-    rand.nextInt(w) - off
+    from + rand.nextInt(w)
   }
-  
+
   def createController: Controller = {
     val behav = new Behaviour() {
+
+      var cnt = 0
+
       def apply(sens: Sensors, player: Player): Unit = {
+        if (cnt % 20 == 0) player.move(ran(-20, 20), ran(-20, 20))
+        cnt += 1
         val vp = player.asInstanceOf[VsocPlayer]
         val pos = vp.getPosition
         val dir = vp.getDirection
         println("" + pos + dir + sens.getFlagsCenter + sens.getFlagsLeft + sens.getFlagsRight)
-        player.move(ran(-60, 60), ran(-50, 50))
-        player.turn(ran(0, 360))
+        player.dash(ran(50, 300))
+        player.turn(ran(-30, 30))
       }
       def getChild(): Optional[Behaviour] = Optional.empty()
       def shouldBeApplied(sens: Sensors): Boolean = true
@@ -88,21 +88,20 @@ class FieldFrame extends JFrame with WindowListener {
 }
 
 class FieldContentPanel(sim: Server) extends JPanel with ActionListener {
-
-  setLayout(new BorderLayout())
+  
   val field = new FieldPanel()
   field.setSim(sim)
-
-  val ctrlPanel = new JPanel()
-  ctrlPanel.setLayout(new FlowLayout())
 
   val takeStepButton = new JButton("take step")
   takeStepButton.addActionListener(this)
 
+  val ctrlPanel = new JPanel()
+  ctrlPanel.setLayout(new FlowLayout())
   ctrlPanel.add(takeStepButton)
 
-  add(field, BorderLayout.CENTER)
-  add(ctrlPanel, BorderLayout.SOUTH)
+  this.setLayout(new BorderLayout())
+  this.add(field, BorderLayout.CENTER)
+  this.add(ctrlPanel, BorderLayout.SOUTH)
 
   def actionPerformed(evt: ActionEvent): Unit = {
     sim.takeStep()
