3589f1dca479df53f9a29ac789837e76d9e7da0e
wwagner4 <wwagner4@gmail.com>
2017-02-25 17:53:21 +0100

    creating of data works fine

--- machinelearning-2017/playerpos/src/main/scala/playerpos/PlayerposMain.scala
+++ machinelearning-2017/playerpos/src/main/scala/playerpos/PlayerposMain.scala
@@ -1,37 +1,14 @@
 package playerpos
 
-import vsoc.behaviour.Behaviour
-import atan.model.Player
-import vsoc.behaviour.Sensors
-import java.util.Optional
-import com.sun.security.sasl.ServerFactoryImpl
-import vsoc.server.Server
-import vsoc.behaviour.BehaviourController
-import vsoc.server.ServerUtil
-import scala.collection.JavaConversions._
+import scala.collection.JavaConversions.asScalaBuffer
 
+import vsoc.server.ServerUtil
 
 object Main extends App {
 
-  
-  val behav = new Behaviour() {
-    
-    def shouldBeApplied(sensors: Sensors): Boolean = true
-    
-    def apply(sensors: Sensors,player: Player): Unit = {
-      
-      player.move(0, 10)
-    }
-
-    def getChild(): Optional[Behaviour] = Optional.empty()
-    
-  }
-  
-  val ctrl = new BehaviourController(behav)
-  
   val srv = ServerUtil.current().createServer(10, 10)
   srv.getPlayers.foreach { p => p.setController(Playerpos.createController) }
-  
+
   for (i <- (1 to 1000)) {
     srv.takeStep()
   }
