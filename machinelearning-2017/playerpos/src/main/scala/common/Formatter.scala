5bf1c2d356b2c11e9cb0af1c5e823e0dd618bb10
wwagner4 <wwagner4@gmail.com>
2017-02-28 08:22:35 +0100

    added separator to outfield

--- machinelearning-2017/playerpos/src/main/scala/common/Formatter.scala
+++ machinelearning-2017/playerpos/src/main/scala/common/Formatter.scala
@@ -4,12 +4,15 @@ import vsoc.util.Vec2D
 
 object Formatter {
 
+  private val sepa = ','
+  private val sepaStr =  "" + ','
+
   def format(a: Array[Double]): String = {
-    a.toList.map { d => f"$d%10.2f" }.mkString(" ")
+    a.toList.map { d => f"$d%10.2f" }.mkString(sepaStr)
   }
 
   def format(pos: Vec2D, dir: Double, a: Array[Double]): String = {
-    f"${pos.getX}%10.2f ${pos.getY}%10.2f $dir%10.2f ${format(a)}"
+    f"${pos.getX}%10.2f$sepaStr${pos.getY}%10.2f$sepaStr$dir%10.2f$sepaStr${format(a)}"
   }
 
 }
