93f95d4d9d444d34559f3af89bca61c9b34e56bc
wwagner4 <wwagner4@gmail.com>
2017-03-04 12:49:35 +0100

    added test data

--- machinelearning-2017/playerpos/src/test/scala/common/VizTryout.scala
+++ machinelearning-2017/playerpos/src/test/scala/common/VizTryout.scala
@@ -4,7 +4,24 @@ object VizTryout extends App {
 
   implicit val creator = VizCreatorGnuplot(Util.scriptsDir)
 
-  val dia = Viz.Diagram("a", "Test A", "text x", "test y")
+  var dataRows = List(
+    Viz.DataRow("a", List(
+      Viz.XY(1, 3),
+      Viz.XY(2, 1),
+      Viz.XY(3, 2),
+      Viz.XY(4, 1),
+      Viz.XY(5, 2)
+    )),
+    Viz.DataRow("b", List(
+      Viz.XY(1, 3),
+      Viz.XY(2, 4),
+      Viz.XY(3, 3),
+      Viz.XY(4, 2),
+      Viz.XY(5, 6)
+    ))
+  )
+
+  val dia = Viz.Diagram("a", "Test A", "text x", "test y", dataRows)
 
   Viz.createDiagram(dia)
 
