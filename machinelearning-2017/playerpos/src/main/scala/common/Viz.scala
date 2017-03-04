f500a3dd5204bac00eb504e454cfb4199be63d38
wwagner4 <wwagner4@gmail.com>
2017-03-04 11:32:43 +0100

    printing simple example works fine

--- machinelearning-2017/playerpos/src/main/scala/common/Viz.scala
+++ machinelearning-2017/playerpos/src/main/scala/common/Viz.scala
@@ -44,8 +44,14 @@ case class VizCreatorGnuplot(outDir: File) extends VizCreator {
   def createDiagram(dia: Diagram): Unit = {
     val script =
       """
-        |
-        |
+        |set terminal pngcairo  transparent enhanced font "arial,10" fontscale 1.0 size 600, 400
+        |set output 'a.png'
+        |set key inside left top vertical Right noreverse enhanced autotitle box lt black linewidth 1.000 dashtype solid
+        |set minussign
+        |set samples 50, 50
+        |set title "Simple Plots"
+        |set title  font ",20" norotate
+        |plot [-10:10] sin(x),atan(x),cos(atan(x))
       """.stripMargin
     val id = dia.id
     val filename = s"diagram_$id.gnuplot"
