01050fb1df2706bf53b9d7e6449672117fceb606
wwagner4 <wwagner4@gmail.com>
2017-03-04 12:40:09 +0100

    split all to lists

--- machinelearning-2017/playerpos/src/main/scala/common/Viz.scala
+++ machinelearning-2017/playerpos/src/main/scala/common/Viz.scala
@@ -43,41 +43,51 @@ case class VizCreatorGnuplot(outDir: File) extends VizCreator {
 
   def createDiagram(dia: Diagram): Unit = {
 
-    val data =
+    val values1 = List(
+      "11 22",
+      "44 55",
+      "77 80"
+    ).mkString("\n")
+
+    val values2 = List(
+      "11 33",
+      "44 66",
+      "50 19",
+      "51 91",
+      "53 12",
+      "57 93",
+      "60 94",
+      "77 10"
+    ).mkString("\n")
+
+    val data = List(
       s"""
-        |$$Mydata << EOD
-        |11 22
-        |44 55
-        |77 80
-        |EOD
-        |$$Mydata1 << EOD
-        |11 33
-        |44 66
-        |50 99
-        |51 91
-        |53 92
-        |57 93
-        |60 94
-        |77 10
-        |EOD
-        |""".stripMargin.trim
+         |$$Mydata0 << EOD
+         |$values1
+         |EOD
+         |""".stripMargin.trim,
+      s"""
+         |$$Mydata1 << EOD
+         |$values2
+         |EOD
+         |""".stripMargin.trim).mkString("\n")
 
     val series = List(
-      s"""$$Mydata using 1:2 title 'a dat' with lines""",
+      s"""$$Mydata0 using 1:2 title 'a dat' with lines""",
       s"""$$Mydata1 using 1:2 title 'b  dat' with impulses"""
     ).mkString(", \\\n")
 
 
     val script =
       s"""
-        |set terminal pngcairo  transparent enhanced font "arial,10" fontscale 1.0 size 600, 400
-        |set output 'a.png'
-        |set key inside left top vertical Right noreverse enhanced autotitle box lt black linewidth 1.000 dashtype solid
-        |set minussign
-        |$data
-        |plot \\
-        |$series
-        |""".stripMargin
+         |set terminal pngcairo  transparent enhanced font "arial,10" fontscale 1.0 size 600, 400
+         |set output 'a.png'
+         |set key inside left top vertical Right noreverse enhanced autotitle box lt black linewidth 1.000 dashtype solid
+         |set minussign
+         |$data
+         |plot \\
+         |$series
+         |""".stripMargin
 
     val id = dia.id
     val filename = s"diagram_$id.gnuplot"
