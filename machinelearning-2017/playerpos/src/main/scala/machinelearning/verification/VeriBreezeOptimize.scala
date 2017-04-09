9b1150f1b0ff9eaf93b7455ffcafb607248d63c4
wwagner4 <wwagner4@gmail.com>
2017-04-09 08:41:26 +0200

    can optimise all combinations

--- machinelearning-2017/playerpos/src/main/scala/machinelearning/verification/VeriBreezeOptimize.scala
+++ machinelearning-2017/playerpos/src/main/scala/machinelearning/verification/VeriBreezeOptimize.scala
@@ -27,14 +27,12 @@ object VeriBreezeOptimize {
 
 class LinRegDiffFunction(x: DenseMatrix[Double], y: DenseMatrix[Double]) extends DiffFunction[DenseVector[Double]] {
 
-  val y1 = y.t.toDenseVector
-  val m = x.rows
+  private val y1 = y.t.toDenseVector
+  private val m = x.rows
 
   def calculate(theta: DenseVector[Double]): (Double, DenseVector[Double]) = {
     val h = x * theta
-
     val c = sum((h - y1) ^:^ 2.0) / (2 * m)
-
     val d = (x.t * (h - y1)) *:* (1.0 / m)
     (c, d)
   }
@@ -46,29 +44,30 @@ object VeriBreezeOptimizeMain extends App {
 
   import VeriBreezeOptimize._
 
-  val grade = 3
-
-  val (_, fname) = datasets(0)
-  val (x, y) = VeriUtil.readDataSet(fname)
-  val x1 = VeriUtil.polyExpand(x, grade)
-
-  val f = new LinRegDiffFunction(x1, y)
-  val fa = new ApproximateGradientFunction[Int, DenseVector[Double]](cost(x1, y) _)
-
-
-  val t = DenseVector.zeros[Double](grade + 1)
-
+  val grades = List(1, 2, 3)
+  val datasetIndexes = List(0, 1, 2, 3)
   val maxIters = List(2, 5, 6, 7, 8, 9, 10, 20, 50, 60, 70, 80, 90, 100, 150)
 
-  val thetas = maxIters.map { mi =>
-    val lbfgs = new LBFGS[DenseVector[Double]](maxIter = mi, m = 3)
-    (mi, lbfgs.minimize(f, t), lbfgs.minimize(fa, t))
+  val thetas = for (grade <- grades; datasetIndex <- datasetIndexes; maxIter <- maxIters) yield {
+    val (datasetSize, fname) = datasets(datasetIndex)
+    val (x, y) = VeriUtil.readDataSet(fname)
+    val x1 = VeriUtil.polyExpand(x, grade)
+
+    val f = new LinRegDiffFunction(x1, y)
+    val fa = new ApproximateGradientFunction[Int, DenseVector[Double]](cost(x1, y)(_))
+
+    val thetaInitial = DenseVector.zeros[Double](grade + 1)
+
+    val lbfgs = new LBFGS[DenseVector[Double]](maxIter = maxIter, m = 3)
+    (datasetSize, grade, maxIter, lbfgs.minimize(f, thetaInitial), lbfgs.minimize(fa, thetaInitial))
+
   }
 
-  thetas.foreach { case (mi, t, ta) =>
+  thetas.foreach { case (s, g, mi, t, ta) =>
     val tStr = common.Formatter.format(t.toArray)
     val taStr = common.Formatter.format(ta.toArray)
-    println(f"$mi%10d | $tStr | $taStr") }
+    println(f"$s%10d $g%10d $mi%10d | $tStr | $taStr")
+  }
 }
 
 
@@ -83,7 +82,7 @@ object CompareToApproximationMain extends App {
   val x1 = VeriUtil.polyExpand(x, grade)
 
   val f = new LinRegDiffFunction(x1, y)
-  val fa = new ApproximateGradientFunction[Int, DenseVector[Double]](cost(x1, y) _)
+  val fa = new ApproximateGradientFunction[Int, DenseVector[Double]](cost(x1, y)(_))
 
   val th = DenseVector(0.001, 0.1, -0.02, 0.01)
 
