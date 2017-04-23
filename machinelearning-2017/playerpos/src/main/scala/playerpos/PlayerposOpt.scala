e11e33094678496a41992a3b93a831ef017d79f8
wwagner4 <wwagner4@gmail.com>
2017-04-23 08:01:47 +0200

    some tryouts working fine

--- machinelearning-2017/playerpos/src/main/scala/playerpos/PlayerposOpt.scala
+++ machinelearning-2017/playerpos/src/main/scala/playerpos/PlayerposOpt.scala
@@ -1,7 +1,10 @@
 package playerpos
 
+import breeze.linalg.DenseMatrix.fill
 import breeze.linalg._
 import breeze.numerics.sin
+import breeze.optimize.ApproximateGradientFunction
+import common.Util
 
 object PlayerposOpt {
 
@@ -10,18 +13,18 @@ object PlayerposOpt {
                       size: Int
                     )
 
-  List(
+  val datasets = List(
     Dataset("pos_1000.txt", 1000),
     Dataset("pos_5000.txt", 5000),
     Dataset("pos_10000.txt", 10000),
     Dataset("pos_50000.txt", 50000)
   )
 
-  def cost(hypothesis: (DenseMatrix[Double], DenseVector[Double]) => DenseVector[Double], theta: DenseVector[Double])(x: DenseMatrix[Double], y: DenseVector[Double]): Double = {
+  type Hypothesis = (DenseMatrix[Double], DenseVector[Double]) => DenseVector[Double]
+
+  def cost(x: DenseMatrix[Double], y: DenseVector[Double])(hypothesis: Hypothesis)(theta: DenseVector[Double]): Double = {
     val m = x.rows
     val h = hypothesis(x, theta)
-    println("-- h -")
-    println(h)
     sum((h - y) ^:^ 2.0) / (2 * m)
   }
 
@@ -35,18 +38,81 @@ object PlayerposOpt {
     sin(x * vd + c) * b + a
   }
 
+  def diffFunctionApprox(x: DenseMatrix[Double], y: DenseVector[Double]) = {
+    val fcost = PlayerposOpt.cost(x, y)(PlayerposOpt.hypothesisSin _)(_)
+    new ApproximateGradientFunction[Int, DenseVector[Double]](fcost)
+  }
+
+  def readDataSet(yColIndex: Int)(fileName: String): (DenseMatrix[Double], DenseVector[Double]) = {
+    val file = Util.dataFile(fileName)
+    val all = csvread(file, separator = ',')
+    val x = all(::, 3 to 44)
+    val y = all(::, yColIndex to yColIndex)
+    val x1 = DenseMatrix.horzcat(fill(x.rows, 1)(1.0), x)
+    (x1, y.toDenseVector)
+  }
+
+  def readDataSetX = readDataSet(0)(_)
+
+  def readDataSetY = readDataSet(1)(_)
+
+  def readDataSetDir = readDataSet(2)(_)
+
+}
+
+object TryoutApproxGradTryout extends App {
+  val x = DenseMatrix((0.0, 0.0, 0.0, 0.0, 0.0), (3.0, 3.0, 4.0, 5.0, 6.0))
+  println("----- x -")
+  println(x)
+  val y = DenseVector(0.0, 1.0)
+  println("----- y -")
+  println(y)
+
+  val fa = PlayerposOpt.diffFunctionApprox(x, y)
+
+  val thetas = List(
+    DenseVector(1.0, 1.0, math.Pi / 2.0, 2.0),
+    DenseVector(1.0, 1.0, math.Pi, 2.0),
+    DenseVector(1.0, 1.0, 0.0, 2.0),
+    DenseVector(1.0, 1.0, - math.Pi / 2.0, 2.0),
+    DenseVector(1.0, 1.0, math.Pi, 2.0)
+  )
+
+  thetas.foreach(theta => println(fa(theta)))
+
+}
+
+object TryoutReadData extends App {
+
+  import PlayerposOpt._
+
+  val Dataset(filename, _) = datasets(2)
+  println(filename)
+  val (x, y) = readDataSetDir(filename)
+  println(s"x:(${x.rows}, ${x.cols})")
+  println(s"y.len=${y.length}")
+  val ystart = y.data.take(10).mkString(", ")
+  println(s"y start [$ystart, ...]")
 }
 
 object TryoutHypothesisSin extends App {
   val x = DenseMatrix((0.0, 0.0, 0.0, 0.0, 0.0), (3.0, 3.0, 4.0, 5.0, 6.0))
+  println("----- x -")
   println(x)
 
-  val theta = DenseVector(1.0, 1.0, math.Pi / 2.0, 2.0)
+  val thetas = List(
+    DenseVector(1.0, 1.0, math.Pi / 2.0, 2.0),
+    DenseVector(1.0, 1.0, math.Pi, 2.0),
+    DenseVector(1.0, 1.0, 0.0, 2.0),
+    DenseVector(1.0, 1.0, - math.Pi / 2.0, 2.0),
+    DenseVector(1.0, 1.0, math.Pi, 2.0)
+  )
 
-  val hyp = PlayerposOpt.hypothesisSin(x, theta)
-
-  println("----- hyp -")
-  println(hyp)
+  thetas.foreach{theta =>
+    val hyp = PlayerposOpt.hypothesisSin(x, theta)
+    println("----- hyp -")
+    println(hyp)
+  }
 
 }
 
@@ -57,7 +123,7 @@ object TryoutCostSin extends App {
   val theta = DenseVector(1.0, 1.0, 0.0, 2.0)
 
   val h = PlayerposOpt.hypothesisSin _
-  val cost = PlayerposOpt.cost(h, theta)(x, y)
+  val cost = PlayerposOpt.cost(x, y)(h)(theta)
 
   println("----- cost -")
   println(cost)
