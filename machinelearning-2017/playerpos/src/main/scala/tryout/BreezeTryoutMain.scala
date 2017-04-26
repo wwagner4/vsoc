eeb6d1bd771c0fbccdce169a094e4e9520c07965
wwagner4 <wwagner4@gmail.com>
2017-04-26 08:35:35 +0200

    sin ext running but wrong results

--- machinelearning-2017/playerpos/src/main/scala/tryout/BreezeTryoutMain.scala
+++ machinelearning-2017/playerpos/src/main/scala/tryout/BreezeTryoutMain.scala
@@ -1,52 +1,84 @@
 package tryout
 
 import breeze.linalg._
+import breeze.numerics.sin
 
 object BreezeTryoutMain extends App {
-  
-  // columnsOfMatrix
-  // allColumnsOfMatrix
-  // createMatrixFromVectors
-  splitVectorInSubvectors
-  
-  def splitVectorInSubvectors {
-    val v = DenseVector(1.0, 2.0, 3.0, 4.0, -1.0, -.9)
-    val a = v.data
-    val splitVectors: Iterator[DenseVector[Double]] = v.data.grouped(3).map { d => DenseVector(d) }
-    for (sub <- splitVectors) { println( s" - split vector $sub}") }
+
+  // columnsOfMatrix()
+  // allColumnsOfMatrix()
+  // createMatrixFromVectors()
+  // splitVectorInSubvectors()
+  // summarizeRowsOfMatrix()
+  matrixMutable()
+
+  def matrixMutable(): Unit = {
+    println("BreezeTryoutMain::matrixMutable")
+    val m = DenseMatrix((0.1, 3.2), (0.7, 3.1), (0.4, 5.2))
+    println(m)
+    val vecs = for (i <- 0 until m.cols) yield {
+      val v = m(::, i)
+      sin(v)
+    }
+    val x1 = DenseMatrix(vecs:_*)
+    println("----- 1 -")
+    println(x1)
+    println(m)
+    val x2 = sum(x1(*, ::))
+    println("----- 2 -")
+    println(x1)
+    println(x2)
+    println(m)
+    // Conclusion: m seems not to be muted
+
   }
-  
-  def createMatrixFromVectors {
-    val v1 = DenseVector(1.2, 2.4, 1.1) 
+
+  def summarizeRowsOfMatrix(): Unit = {
+    val m = DenseMatrix((0.1, 3.2), (0.7, 3.1), (0.4, 5.2))
+    println(m)
+    val ms = sum(m(*, ::))
+    println(ms)
+  }
+
+  def splitVectorInSubvectors(): Unit = {
+    val v = DenseVector(1.0, 2.0, 3.0, 4.0, -1.0, -.9)
+    val splitVectors: Iterator[DenseVector[Double]] = v.data.grouped(3).map { d => DenseVector(d) }
+    for (sub <- splitVectors) {
+      println(s" - split vector $sub}")
+    }
+  }
+
+  def createMatrixFromVectors(): Unit = {
+    val v1 = DenseVector(1.2, 2.4, 1.1)
     val v2 = DenseVector(2.2, 2.4, 2.1)
     val m = DenseMatrix(v1, v2) // <- Vectors are the rows of the new matrix
     println(m)
   }
-  
-  def allColumnsOfMatrix {
+
+  def allColumnsOfMatrix(): Unit = {
     println("Breeze tryout :: all columns of matrix")
     val m = DenseMatrix((0.1, 3.2), (0.7, 3.1), (0.4, 5.2))
-    for (i <- (0 until m.cols)) {
-      val c = m((0 until m.rows),i)
+    for (i <- 0 until m.cols) {
+      val c = m(0 until m.rows, i)
       println(c)
     }
   }
-  
-  def columnsOfMatrix {
+
+  def columnsOfMatrix(): Unit = {
     println("Breeze tryout :: column of matrix")
     val m = DenseMatrix((0.1, 3.2), (0.7, 3.1), (0.4, 5.2))
     println("---- m - all")
-    println(m.getClass.getName)   	
-    println(m) 
-  
+    println(m.getClass.getName)
+    println(m)
+
     val m0 = m(::, 0)
     println("---- m0 - all")
-    println(m0.getClass.getName)   	
-    println(m0) 
-  
+    println(m0.getClass.getName)
+    println(m0)
+
     val m1 = m(::, 1)
     println("---- m1 - all")
-    println(m1.getClass.getName)   	
-    println(m1) 
+    println(m1.getClass.getName)
+    println(m1)
   }
 }
