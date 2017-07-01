c9ecaba16ca6c559ac418783457ea6c184bcd423
wwagner4 <wwagner4@gmail.com>
2017-07-02 00:43:48 +0200

    can create a dataset

--- ml-2017-java/src/main/java/vsoc/ml/DataSetReader.java
+++ ml-2017-java/src/main/java/vsoc/ml/DataSetReader.java
@@ -4,6 +4,7 @@ import org.apache.spark.SparkConf;
 import org.apache.spark.api.java.JavaRDD;
 import org.apache.spark.api.java.JavaSparkContext;
 import org.datavec.api.records.reader.RecordReader;
+import org.datavec.api.records.reader.impl.collection.CollectionRecordReader;
 import org.datavec.api.records.reader.impl.csv.CSVRecordReader;
 import org.datavec.api.transform.TransformProcess;
 import org.datavec.api.transform.schema.Schema;
@@ -11,10 +12,13 @@ import org.datavec.api.writable.Writable;
 import org.datavec.spark.transform.SparkTransformExecutor;
 import org.datavec.spark.transform.misc.StringToWritablesFunction;
 import org.datavec.spark.transform.misc.WritablesToStringFunction;
+import org.deeplearning4j.datasets.datavec.RecordReaderDataSetIterator;
+import org.nd4j.linalg.dataset.DataSet;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import java.io.File;
+import java.io.Serializable;
 import java.util.ArrayList;
 import java.util.Collection;
 import java.util.List;
@@ -29,7 +33,7 @@ public class DataSetReader {
 
     public static void main(String... arg) {
         String baseDirName = "/Users/wwagner4/vsoc/data";
-        String dataFileName = "random_pos_10.csv";
+        String dataFileName = "random_pos_100.csv";
 
         new DataSetReader().readPlayerposXDataSet(baseDirName, dataFileName);
     }
@@ -61,34 +65,44 @@ public class DataSetReader {
 
         JavaSparkContext sc = new JavaSparkContext(conf);
 
-        log.info("Created Spark Context: " + sc);
-
         JavaRDD<String> inputRdd = sc.textFile(file.getAbsolutePath());
-
-        System.out.println("\n\n---- Original Data ----");
-        List<String> inputLines = inputRdd.collect();
-        for(String s : inputLines) System.out.println(s);
 
         //We first need to parse this format. It's comma-delimited (CSV) format, so let's parse it using CSVRecordReader:
         RecordReader recordReader = new CSVRecordReader(0, ",");
         JavaRDD<List<Writable>> parsedInputData = inputRdd.map(new StringToWritablesFunction(recordReader));
         JavaRDD<List<Writable>> processedData = SparkTransformExecutor.execute(parsedInputData, tp);
 
-        // Prepare processed data for output
+        writeToStdout(processedData);
+
+        CollectionRecordReader reader = new CollectionRecordReader(processedData.collect());
+
+        RecordReaderDataSetIterator dataSetIterator = new RecordReaderDataSetIterator(reader,10, 42, 42, true);
+        log.info("iterator: " + dataSetIterator);
+        while (dataSetIterator.hasNext()) {
+            DataSet dataSet = dataSetIterator.next();
+            log.info("META:" + dataSet.toString());
+        }
+        log.info("showed iterations");
+    }
+
+    void writeToStdout(JavaRDD<List<Writable>> processedData) {
         JavaRDD<String> processedAsString = processedData.map(new WritablesToStringFunction(","));
+        writeStringToStdout(processedAsString);
+    }
 
+    void writeStringToStdout(JavaRDD<String> stringRdd) {
         // Write processed data to stdout
-        System.out.println("\n\n---- Processed Data ----");
-        List<String> outputLines = processedAsString.collect();
-        for(String s : outputLines) System.out.println(s);
+        List<String> outputLines = stringRdd.collect();
+        for (String s : outputLines) System.out.println(s);
+    }
 
-        // Write processed data to txt-file
+    void writeToFile(JavaRDD<List<Writable>> processedData) {
+        JavaRDD<String> stringRdd = processedData.map(new WritablesToStringFunction(","));
         File tmpDir = new File(System.getProperty("java.io.tmpdir"));
         UUID uuid = UUID.randomUUID();
         File outDir = new File(tmpDir, "playerpos-" + uuid);
-        processedAsString.saveAsTextFile("file://" + outDir);
+        stringRdd.saveAsTextFile("file://" + outDir);
         log.info("Wrote processed data to txt-file: " + outDir);
-
     }
 
     protected Schema createPlayerposSchema() {
