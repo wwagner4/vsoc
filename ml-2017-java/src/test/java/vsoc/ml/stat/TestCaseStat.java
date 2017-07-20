d69eed7ca62777a0d5c616384e7f8c2891ae7bae
wwagner4 <wwagner4@gmail.com>
2017-07-20 08:25:45 +0200

    Started Stat testcase

--- ml-2017-java/src/test/java/vsoc/ml/stat/TestCaseStat.java
+++ ml-2017-java/src/test/java/vsoc/ml/stat/TestCaseStat.java
@@ -1,6 +1,8 @@
 package vsoc.ml.stat;
 
+import org.apache.commons.math.stat.descriptive.DescriptiveStatistics;
 import org.junit.Test;
+import org.nd4j.linalg.api.ndarray.INDArray;
 
 import static org.junit.Assert.*;
 
@@ -8,7 +10,13 @@ public class TestCaseStat {
 
     @Test
     public void test() {
-        fail("OK");
+        INDArray diffs = createDiffs();
+        Stat stat = new Stat(diffs);
+        System.out.println(stat.toString());
+    }
+
+    private INDArray createDiffs() {
+        throw new IllegalStateException("NOT YET IMPLEMENTED");
     }
 
 }
