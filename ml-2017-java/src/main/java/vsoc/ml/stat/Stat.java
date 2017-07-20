ce190e9f0823d02c12d2e343857c9b70464d0fcc
wwagner4 <wwagner4@gmail.com>
2017-07-20 08:21:54 +0200

    Stat compileable. Added dependency fpr apache math

--- ml-2017-java/src/main/java/vsoc/ml/stat/Stat.java
+++ ml-2017-java/src/main/java/vsoc/ml/stat/Stat.java
@@ -1,48 +1,28 @@
 package vsoc.ml.stat;
 
+import org.apache.commons.math3.stat.descriptive.DescriptiveStatistics;
 import org.nd4j.linalg.api.ndarray.INDArray;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
-import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.List;
-import java.util.Optional;
 
 /**
  * Calculates some statistics about INDArrays
  */
 public class Stat {
 
-    private INDArray data;
+    private static final Logger log = LoggerFactory.getLogger(Stat.class);
 
-    private Optional<INDArray> mean = Optional.empty();
-
-    private Optional<INDArray> stdev = Optional.empty();
+    private DescriptiveStatistics data;
 
     public Stat(INDArray data) {
-        this.data = data;
-    }
+        int[] shape = data.shape();
 
-    public List<Pair<String, Number>> stat() {
-        List<Pair<String, Number>> re = new ArrayList<>();
-        re.add(new Pair("mean", mean()));
-        re.add(new Pair("stdev", stdev()));
-        return re;
-    }
+        log.info("shape of data " + Arrays.toString(shape));
 
-    private Number stdev() {
-        return this.stdev.orElse(calcStdev());
-    }
-
-    private Number calcStdev() {
-        if (data.shape().length == 2 && data.shape()[1] == 1) {
-            return data.sumNumber();
-        } else {
-            throw new IllegalStateException("Currently only NDArrays with shape [n, 1] are supported. " + Arrays.toString(data.shape()));
-        }
-    }
-
-    private Number mean() {
 
     }
+
 
 }
---
+++ ml-2017-java/src/test/java/vsoc/ml/stat/TestCaseStat.java
@@ -0,0 +1,14 @@
+package vsoc.ml.stat;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class TestCaseStat {
+
+    @Test
+    public void test() {
+        fail("OK");
+    }
+
+}
