e7fcf89affdce2aa1b3a0a0e186bb1d6e63d12f2
wwagner4 <wwagner4@gmail.com>
2017-08-03 08:30:21 +0200

    added testcase

--- machinelearning-2017/dl4j-training/src/test/scala/vsoc/training/UtilTrainingSuite.scala
+++ machinelearning-2017/dl4j-training/src/test/scala/vsoc/training/UtilTrainingSuite.scala
@@ -12,6 +12,29 @@ class UtilTrainingSuite extends FunSuite {
 
   val eps = 0.000001
 
+  test("convert to xy 0 1") {
+    val array = Nd4j.create(Array(
+      Array(1.0, 2.0, 3.8, 4.4),
+      Array(1.2, 2.7, 3.9, 4.4),
+      Array(1.3, 2.6, 3.0, 4.4),
+      Array(1.4, 2.5, 3.2, 4.5)
+    ))
+    val out = convertXY(array, (0, 1))
+    assert(out.size === 4)
+
+    out(0).x.doubleValue() should equal(1.0 +- eps)
+    out(0).y.doubleValue() should equal(2.0 +- eps)
+
+    out(1).x.doubleValue() should equal(1.2 +- eps)
+    out(1).y.doubleValue() should equal(2.7 +- eps)
+
+    out(2).x.doubleValue() should equal(1.3 +- eps)
+    out(2).y.doubleValue() should equal(2.6 +- eps)
+
+    out(3).x.doubleValue() should equal(1.4 +- eps)
+    out(3).y.doubleValue() should equal(2.5 +- eps)
+  }
+
   test("convert to xyz 0 1 3") {
     val array = Nd4j.create(Array(
       Array(1.0, 2.0, 3.8, 4.4),
