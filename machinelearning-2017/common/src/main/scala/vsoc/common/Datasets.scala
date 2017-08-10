672399741151aa6ec7a512c16ba0411d2508b2cc
wwagner4 <wwagner4@gmail.com>
2017-08-10 08:18:18 +0200

    finished common dataset

--- machinelearning-2017/common/src/main/scala/vsoc/common/Datasets.scala
+++ machinelearning-2017/common/src/main/scala/vsoc/common/Datasets.scala
@@ -2,18 +2,34 @@ package vsoc.common
 
 class Datasets {
 
-  sealed trait Sizes {def size: Int}
+  sealed trait Size {def size: Int}
 
-  case object Sizes_10 extends Sizes {val size = 10}
-  case object Sizes_100 extends Sizes {val size = 100}
-  case object Sizes_1000 extends Sizes {val size = 1000}
-  case object Sizes_5000 extends Sizes {val size = 5000}
-  case object Sizes_10000 extends Sizes {val size = 10000}
-  case object Sizes_50000 extends Sizes {val size = 50000}
-  case object Sizes_100000 extends Sizes {val size = 100000}
-  case object Sizes_500000 extends Sizes {val size = 500000}
-  case object Sizes_1000000 extends Sizes {val size = 1000000}
-  case object Sizes_5000000 extends Sizes {val size = 5000000}
-  case object Sizes_10000000 extends Sizes {val size = 100000000}
+  case object Size_10 extends Size {val size = 10}
+  case object Size_100 extends Size {val size = 100}
+  case object Size_1000 extends Size {val size = 1000}
+  case object Size_5000 extends Size {val size = 5000}
+  case object Size_10000 extends Size {val size = 10000}
+  case object Size_50000 extends Size {val size = 50000}
+  case object Size_100000 extends Size {val size = 100000}
+  case object Size_500000 extends Size {val size = 500000}
+  case object Size_1000000 extends Size {val size = 1000000}
+  case object Size_5000000 extends Size {val size = 5000000}
+  case object Size_10000000 extends Size {val size = 100000000}
+
+  sealed trait Id {def code: String}
+
+  case object Id_A extends Id {val code = "A"}
+  case object Id_B extends Id {val code = "B"}
+  case object Id_C extends Id {val code = "C"}
+  case object Id_D extends Id {val code = "D"}
+  case object Id_E extends Id {val code = "E"}
+  case object Id_F extends Id {val code = "F"}
+
+  sealed trait Data  {def code: String}
+
+  case object Data_PLAYERPOS_X extends Data {val code = "playerpos_x"}
+
+  def filename(data: Data, id: Id, size: Size) = s"vsoc_${data.code}_${id.code}_${size.size}.csv"
 
 }
+
--- machinelearning-2017/dl4j-training/src/main/scala/vsoc/training/LearningRateIterationsTraining.scala
+++ machinelearning-2017/dl4j-training/src/main/scala/vsoc/training/LearningRateIterationsTraining.scala
@@ -15,14 +15,14 @@ object LearningRateIterationsTraining extends App {
       iterations = 50)
   }
 
-  val sizeTrainingDatas = List(100, 50000, 200000, 500000, 1000000, 1000000)
+  val sizeTrainingDatas = List(50000, 200000, 500000, 1000000)
 
   val series = sizeTrainingDatas.map{s =>
     val title = "Trainingdata size:" + s
     val mpar = metaParams.map(mp => mp.copy(sizeTrainingData = s, batchSizeTrainingData = s / 10))
     MetaParamSeries(title, mpar)}
 
-  val run = MetaParamRun(imgWidth = 1800, imgHeight = 1200, columns = 3, series = series)
+  val run = MetaParamRun(imgWidth = 1000, imgHeight = 1200, columns = 2, series = series)
   new Training(log).trainSeries(run)
 
 
