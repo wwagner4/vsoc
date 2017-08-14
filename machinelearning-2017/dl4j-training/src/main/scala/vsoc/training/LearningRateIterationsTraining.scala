76a0ee9fb74bac1d2aaaa8439bcc9639d18a7123
wwagner4 <wwagner4@gmail.com>
2017-08-14 17:28:41 +0200

    refactoring

--- machinelearning-2017/dl4j-training/src/main/scala/vsoc/training/LearningRateIterationsTraining.scala
+++ machinelearning-2017/dl4j-training/src/main/scala/vsoc/training/LearningRateIterationsTraining.scala
@@ -7,39 +7,35 @@ import scala.util.Random
 
 object LearningRateIterationsTraining extends App {
 
-  val _iterations = 50
-
-  val learningRates = Map(0.01 -> "10^-2", 0.001 -> "10^-3", 0.0001 -> "10^-4", 0.00001 -> "10^-5")
-
   val log: Logger = LoggerFactory.getLogger(classOf[Training])
 
-  val metaParams: Seq[MetaParam] = learningRates.keys.toSeq.sorted.reverse.map { value =>
-    MetaParam(
-      seed = Random.nextLong(),
-      learningRate = value,
-      iterations = _iterations,
-      variableParmDescription = () => learningRates(value)
-    )
-  }
+  val _iterations = 50
 
+  val learningRates = Seq((0.01, "10^-2"), (0.001, "10^-3"), (0.0001, "10^-4"), (0.00001, "10^-5"))
   val sizeTrainingDatas = List(Dat.Size_10000, Dat.Size_50000, Dat.Size_100000, Dat.Size_500000)
 
-  val series = sizeTrainingDatas.map{sizeDat =>
-    val mpar = metaParams.map{mp =>
-      val desc = mp.trainingData.copy(size = sizeDat)
-      mp.copy(trainingData = desc)}
+  val  series = for ((lr, lrDesc) <- learningRates) yield {
+    val mpar = for (sizeDat <- sizeTrainingDatas) yield {
+      MetaParam(
+        seed = Random.nextLong(),
+        learningRate = lr,
+        trainingData = Dat.DataDesc(Dat.Data_PLAYERPOS_X, Dat.Id_A, sizeDat),
+        testData = Dat.DataDesc(Dat.Data_PLAYERPOS_X, Dat.Id_B, sizeDat),
+        iterations = _iterations,
+        variableParmDescription = () => sizeDat.size.toString
+      )
+    }
     MetaParamSeries(
-      description = "trainingdata size: " + Formatter.formatNumber("%d", sizeDat.size),
-      descriptionX = "learning rate",
+      description = "learning rate: " + lrDesc,
+      descriptionX = "size",
       metaParams = mpar
     )
-
   }
 
   val run = MetaParamRun(
-    description = Some("Test Learning Rate. Iterations: " + _iterations),
+    description = Some("test learning rate | iterations: " + _iterations),
     imgWidth = 1000,
-    imgHeight = 1200,
+    imgHeight = 1500,
     columns = 2,
     series = series)
 
