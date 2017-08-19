166cc181076eb350e2ef15cd331c611b157eefeb
wwagner4 <wwagner4@gmail.com>
2017-08-19 10:18:20 +0200

    opt algo working

--- machinelearning-2017/dl4j-training/src/main/scala/vsoc/training/OptAlgoTraining.scala
+++ machinelearning-2017/dl4j-training/src/main/scala/vsoc/training/OptAlgoTraining.scala
@@ -1,7 +1,7 @@
 package vsoc.training
 
-import org.slf4j.{Logger, LoggerFactory}
-import vsoc.common.{Dat, Formatter}
+import org.deeplearning4j.nn.api.OptimizationAlgorithm
+import vsoc.common.Dat
 
 import scala.util.Random
 
@@ -10,20 +10,25 @@ object OptAlgoTraining extends App {
   val _iterations = 500
   val _seed = Random.nextLong()
 
-  val learningRates = Seq(0.01, 0.001, 0.0001, 0.00001)
+  val optAlgos = Seq(
+    OptimizationAlgorithm.CONJUGATE_GRADIENT,
+    OptimizationAlgorithm.LBFGS,
+    OptimizationAlgorithm.LINE_GRADIENT_DESCENT,
+    OptimizationAlgorithm.STOCHASTIC_GRADIENT_DESCENT
+  )
   val sizeTrainingDatas = List(Dat.Size_50000, Dat.Size_100000, Dat.Size_500000, Dat.Size_1000000)
 
   val series = for (sizeDat <- sizeTrainingDatas) yield {
-    val  mpar = for (lr <- learningRates) yield {
+    val  mpar = for (_optAlgo <- optAlgos) yield {
       MetaParam(
-        description = s"""sizeDat:$sizeDat - learningRate:${Formatter.formatNumber("%.2E", lr)}""",
+        description = s"""sizeDat:$sizeDat - optAlgo:${_optAlgo}""",
         seed = _seed,
-        learningRate = lr,
         batchSizeTrainingDataRelative = 0.5,
         trainingData = Dat.DataDesc(Dat.Data_PLAYERPOS_X, Dat.Id_A, sizeDat),
         testData = Dat.DataDesc(Dat.Data_PLAYERPOS_X, Dat.Id_B, Dat.Size_1000),
         iterations = _iterations,
-        variableParmDescription = () => Formatter.formatNumber("%.2E", lr)
+        optAlgo = _optAlgo,
+        variableParmDescription = () => formatOptAlgo(_optAlgo)
       )
     }
     MetaParamSeries(
@@ -31,6 +36,16 @@ object OptAlgoTraining extends App {
       descriptionX = "learning rate",
       metaParams = mpar
     )
+  }
+
+  def formatOptAlgo(optAlgo: OptimizationAlgorithm): String = {
+    optAlgo match {
+      case OptimizationAlgorithm.CONJUGATE_GRADIENT => "CG"
+      case OptimizationAlgorithm.LBFGS => "LB"
+      case OptimizationAlgorithm.LINE_GRADIENT_DESCENT => "LG"
+      case OptimizationAlgorithm.STOCHASTIC_GRADIENT_DESCENT => "SG"
+      case _ => "??"
+    }
   }
 
   val run = MetaParamRun(
