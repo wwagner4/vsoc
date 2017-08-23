49045a05532cfc954553492f5dd7f995f4a88f75
wolfgang wagner <wwagner4@gmail.com>
2017-08-23 18:18:44 +0200

    more genetic

--- machinelearning-2017/dl4j-training/src/main/scala/vsoc/training/genetic/GeneticTryout.scala
+++ machinelearning-2017/dl4j-training/src/main/scala/vsoc/training/genetic/GeneticTryout.scala
@@ -2,36 +2,58 @@ package vsoc.training.genetic
 
 import java.util
 
+import org.datavec.api.records.reader.impl.csv.CSVRecordReader
+import org.datavec.api.split.FileSplit
+import org.deeplearning4j.datasets.datavec.RecordReaderDataSetIterator
 import org.deeplearning4j.nn.api.OptimizationAlgorithm
 import org.deeplearning4j.nn.conf.{MultiLayerConfiguration, NeuralNetConfiguration, Updater}
 import org.deeplearning4j.nn.conf.layers.{DenseLayer, OutputLayer}
 import org.deeplearning4j.nn.multilayer.MultiLayerNetwork
 import org.deeplearning4j.nn.weights.WeightInit
 import org.nd4j.linalg.activations.Activation
+import org.nd4j.linalg.dataset.api.iterator.DataSetIterator
 import org.nd4j.linalg.lossfunctions.LossFunctions
+import vsoc.common.Dat
+import vsoc.datavec.playerpos.CreateData
 
 object GeneticTryout extends App {
 
+  val delim = ";"
+
 
   val nnConf = nnConfiguration()
-  val nn = new MultiLayerNetwork(nnConf)
+  val nn: MultiLayerNetwork = new MultiLayerNetwork(nnConf)
   nn.init()
-  println("after init " + nn)
+  println("after init")
+  val data = readPlayerposXDataSet(Dat.DataDesc(Dat.Data_PLAYERPOS_X, Dat.Id_A, Dat.Size_1000), 0.1)
+  println("after readData")
+  printParam(nn)
+  nn.fit(data)
+  println("after fit")
+  printParam(nn)
 
-  for ((l, i) <- nn.getLayers.zipWithIndex) {
-    println(s"l $i numParam: ${l.numParams()}")
-    println(s"l $i W: ${util.Arrays.toString(l.getParam("W").shape())}")
-    println(s"l $i b: ${util.Arrays.toString(l.getParam("b").shape())}")
+  private def printParam(_nn: MultiLayerNetwork) = {
+    for ((l, i) <- _nn.getLayers.zipWithIndex) {
+      println(s"--- layer $i ---")
+      println(s"l $i W: ${util.Arrays.toString(l.getParam("W").shape())}")
+      println(s"l $i W: ${l.getParam("W")}")
+      println(s"l $i b: ${util.Arrays.toString(l.getParam("b").shape())}")
+      println(s"l $i b: ${l.getParam("b")}")
+    }
   }
 
-
-
+  def readPlayerposXDataSet(desc: Dat.DataDesc, batchSizeRelative: Double): DataSetIterator = {
+    val infile = CreateData.createDataFile(desc)
+    val recordReader = new CSVRecordReader(0, delim)
+    recordReader.initialize(new FileSplit(infile))
+    new RecordReaderDataSetIterator(recordReader, (desc.size.size * batchSizeRelative).toInt, 42, 42, true)
+  }
 
   private def nnConfiguration(): MultiLayerConfiguration = {
-    val numHiddenNodes = 12
+    val numHiddenNodes = 50
     new NeuralNetConfiguration.Builder()
       .seed(-1)
-      .iterations(10)
+      .iterations(1)
       .optimizationAlgo(OptimizationAlgorithm.STOCHASTIC_GRADIENT_DESCENT)
       .learningRate(0.001)
       .weightInit(WeightInit.XAVIER)
@@ -40,7 +62,7 @@ object GeneticTryout extends App {
       .momentum(0.9)
       .list
       .layer(0, new DenseLayer.Builder()
-        .nIn(10)
+        .nIn(42)
         .nOut(numHiddenNodes)
         .activation(Activation.TANH).build)
       .layer(1, new DenseLayer.Builder()
