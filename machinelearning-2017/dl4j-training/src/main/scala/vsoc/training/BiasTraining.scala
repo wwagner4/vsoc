c7f94825eabb1b3b83e123722d956ff97852161c
wolfgang wagner <wwagner4@gmail.com>
2017-08-28 18:12:39 +0200

    added new bias training

---
+++ machinelearning-2017/dl4j-training/src/main/scala/vsoc/training/BiasTraining.scala
@@ -0,0 +1,42 @@
+package vsoc.training
+
+import vsoc.common.Dat
+
+import scala.util.Random
+
+object BiasTraining extends App {
+
+  val ids = Seq(Dat.Id_A, Dat.Id_B, Dat.Id_E)
+  val numHiddenNodes = Seq(100, 300, 500)
+  val _seed = Random.nextLong()
+
+  Training().run(
+    MetaParamRun(
+      description = Some("Bias"),
+      descriptionMd = Some(
+        """
+          |Testset to show that the learned bias is independend from the
+          |testdata.
+        """.stripMargin),
+      clazz = BatchSizeTraining.getClass.toString,
+      imgWidth = 2000,
+      imgHeight = 1000,
+      columns = 3,
+      series = for (_num <- numHiddenNodes) yield
+        MetaParamSeries(
+          description = "hidden nodes " + _num,
+          descriptionX = "id",
+          metaParams = for (id <- ids) yield {
+            MetaParam(
+              id = Some(id + "_" + _num),
+              numHiddenNodes = _num,
+              description = s"testset id:$id",
+              seed = _seed,
+              testData = Dat.DataDesc(Dat.Data_PLAYERPOS_X, id, Dat.Size_1000),
+              variableParmDescription = () => id.code
+            )
+          }
+        )
+    )
+  )
+}
