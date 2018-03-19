6370a869fb310d8239ba7ef92064347ab2b42b5d
wwagner4 <wwagner4@gmail.com>
2018-03-19 17:39:24 +0100

    more config possibillities

--- vsoc-ga-2018/analyse/src/main/scala/vsoc/ga/analyse/Data01Main.scala
+++ vsoc-ga-2018/analyse/src/main/scala/vsoc/ga/analyse/Data01Main.scala
@@ -2,12 +2,6 @@ package vsoc.ga.analyse
 
 import vsoc.ga.common.config.Configs
 
-sealed trait DataGa
-
-case class DataGa_One(id: String, nr: String) extends DataGa
-
-case class DataGa_Multi(id: String, title: String, datas: Seq[(String, String)]) extends DataGa
-
 object Data01Main extends App {
 
   val cfg = Configs.bob002
