a78ed95bf0239d367f11ceef6de0a009fa700975
wwagner4 <wwagner4@gmail.com>
2018-03-22 18:04:44 +0100

    added server

---
+++ vsoc-ga-2018/analyse/src/main/scala/vsoc/ga/analyse/server/Server.scala
@@ -0,0 +1,47 @@
+package vsoc.ga.analyse.server
+
+import java.nio.file.{Path, Paths}
+import java.util.concurrent.{Executors, TimeUnit}
+
+import vsoc.ga.analyse.Data01Dia
+import vsoc.ga.common.UtilReflection
+import vsoc.ga.common.config.{Config, Configs}
+
+object Server extends App {
+
+  require(args.length == 3, "Three parameters required")
+
+  val workDir = args(0)
+  val httpDir = args(1)
+  val configsList = args(2)
+
+  val workPath = Paths.get(workDir)
+  require(workPath.isAbsolute, s"'$workPath' must be an absolute path")
+
+  val httpPath = Paths.get(httpDir)
+  require(httpPath.isAbsolute, s"'$httpPath' must be an absolute path")
+
+  val configs: Seq[Config] = configsList.split(",").toSeq.map(getConfig)
+
+  ServerImpl.start(workPath, httpPath, configs)
+
+  def getConfig(name: String): Config = UtilReflection.call(Configs, name, classOf[Config])
+
+}
+
+object ServerImpl {
+
+  def start(workPath: Path, httpPath: Path, cfgs: Seq[Config]): Unit = {
+    val exe = Executors.newScheduledThreadPool(10)
+    exe.scheduleAtFixedRate(() => run(), 5, 10, TimeUnit.SECONDS)
+    println("started server")
+
+    def run(): Unit = {
+      cfgs.foreach{c =>
+        Data01Dia.run(c)
+        println(s"created data for configuration '${c.id}'")
+      }
+    }
+  }
+
+}
---
+++ vsoc-ga-2018/analyse/src/test/scala/vsoc/ga/analyse/server/ServerTryoutWork.scala
@@ -0,0 +1,7 @@
+package vsoc.ga.analyse.server
+
+object ServerTryoutWork extends App {
+
+  Server.main(Array("C:\\ta30\\entw1\\work", "C:\\ta30\\entw1\\work\\http", "work001,work002,work003"))
+
+}
