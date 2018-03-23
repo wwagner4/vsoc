c0dd3ba200e80d7dd9ffdec91c6015b745173809
wwagner4 <wwagner4@gmail.com>
2018-03-23 15:50:09 +0100

    finished server

--- vsoc-ga-2018/analyse/src/main/scala/vsoc/ga/analyse/server/Server.scala
+++ vsoc-ga-2018/analyse/src/main/scala/vsoc/ga/analyse/server/Server.scala
@@ -1,6 +1,9 @@
 package vsoc.ga.analyse.server
 
-import java.nio.file.{Path, Paths}
+import java.nio.file._
+import java.nio.file.attribute.BasicFileAttributeView
+import java.time.format.DateTimeFormatter
+import java.time.{Instant, LocalDateTime, ZoneId}
 import java.util.concurrent.{Executors, TimeUnit}
 
 import vsoc.ga.analyse.{Data01Dia, DiaConf_SUPRESS_TIMESTAMP}
@@ -31,9 +34,61 @@ object Server extends App {
 
 object ServerImpl {
 
+  import collection.JavaConverters._
+
+  def createIndexHtml(httpPath: Path): Unit = {
+    require(Files.exists(httpPath))
+
+    val x = Files.list(httpPath).iterator().asScala.toList.map(_.getFileName)
+
+
+    val imgFiles = Files.list(httpPath).iterator().asScala.toList
+      .filter(p => p.getFileName.toString.endsWith("png"))
+      .map{p =>
+        val attr: BasicFileAttributeView = Files.getFileAttributeView(p, classOf[BasicFileAttributeView])
+        val name = p.getFileName
+        val millis = attr.readAttributes().creationTime().toMillis
+        val  inst = Instant.ofEpochMilli(millis)
+        val ldt = LocalDateTime.ofInstant(inst, ZoneId.systemDefault())
+        val formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss")
+        val creationTimeStr = formatter.format(ldt)
+        s"""<img src="$name"></img><p>creation time: $creationTimeStr</p>"""
+      }.mkString("\n")
+
+    val content = s"""
+      |<html>
+      |<head>
+      |<title>vsoc results</title>
+      |<style>
+      |body {
+      |     font-family: sans-serif;
+      |     font-size: large;
+      |}
+      |h3 {
+      |    text-align: center;
+      |}
+      |</style>
+      |</head>
+      |<body>
+      |<h3>vsoc results</h3>
+      |$imgFiles
+      |<body/>
+      |</html>
+    """.stripMargin
+
+    val file = httpPath.resolve("index.html")
+
+    val pw = Files.newBufferedWriter(file)
+    pw.write(content)
+    pw.close()
+
+    println(s"Wrote to $file")
+
+  }
+
   def start(workPath: Path, httpPath: Path, cfgs: Seq[Config]): Unit = {
     val exe = Executors.newScheduledThreadPool(10)
-    exe.scheduleAtFixedRate(() => run(), 5, 10, TimeUnit.SECONDS)
+    exe.scheduleAtFixedRate(() => run(), 0, 60, TimeUnit.MINUTES)
     println("started server")
 
     def run(): Unit = {
@@ -41,6 +96,7 @@ object ServerImpl {
         Data01Dia.run(c, diaConfs = Seq(DiaConf_SUPRESS_TIMESTAMP), diaDir = Some(httpPath))
         println(s"created data for configuration '${c.id}'")
       }
+      createIndexHtml(httpPath)
     }
   }
 
