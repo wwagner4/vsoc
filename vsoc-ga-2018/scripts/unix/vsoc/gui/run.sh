26739657fd208d80c0893b27c3c0aa0e546eec30
wwagner4 <wwagner4@gmail.com>
2018-03-29 08:32:53 +0200

    removed scripts

--- vsoc-ga-2018/scripts/unix/vsoc/gui/run.sh
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env bash
-SCRIPTDIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
-java -Xmx7g \
- -cp ${SCRIPTDIR}:$HOME/prj/vsoc/vsoc-ga-2018/trainga/target/scala-2.12/trainga-assembly-0.0.1-SNAPSHOT.jar \
- vsoc.ga.trainga.gui.GuiPopulationMain $@
