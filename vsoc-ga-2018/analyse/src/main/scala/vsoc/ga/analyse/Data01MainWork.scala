e7e09fb0e391209fa72aabdf6022fd6129d899d5
wwagner4 <wwagner4@gmail.com>
2018-04-10 13:18:37 +0200

    w006 configs

--- vsoc-ga-2018/analyse/src/main/scala/vsoc/ga/analyse/Data01MainWork.scala
+++ vsoc-ga-2018/analyse/src/main/scala/vsoc/ga/analyse/Data01MainWork.scala
@@ -2,25 +2,22 @@ package vsoc.ga.analyse
 
 import java.nio.file.Paths
 
-import entelijan.viz.Viz
 import vsoc.ga.common.config.Configs
 
 object Data01MainWork extends App {
 
   val configs = Seq(
-    Configs.work001,
-    Configs.work002,
-    Configs.work003,
+    Configs.work006,
   )
 
-  val _wd: String = ???
+  val _wd: String = "C:\\ta30\\entw1\\work"
 
   configs.foreach{c =>
     Data01Dia.run(
       cfg = c,
       workDir = Paths.get(_wd),
-      filterFactor = 10,
-      yRange = Some(Viz.Range(None, Some(100)))
+      //filterFactor = 10,
+      //yRange = Some(Viz.Range(None, Some(100)))
     )
   }
 
--- vsoc-ga-2018/analyse/src/test/scala/vsoc/ga/analyse/server/ServerTryoutWork.scala
+++ vsoc-ga-2018/analyse/src/test/scala/vsoc/ga/analyse/server/ServerTryoutWork.scala
@@ -2,6 +2,6 @@ package vsoc.ga.analyse.server
 
 object ServerTryoutWork extends App {
 
-  Server.main(Array("C:\\ta30\\entw1\\work", "C:\\ta30\\entw1\\work\\http", "work001,work002,work003"))
+  Server.main(Array("C:\\ta30\\entw1\\work", "C:\\ta30\\entw1\\work\\http", "work006"))
 
 }
