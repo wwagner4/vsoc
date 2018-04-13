d2cdde38324902cbfc1183055e874a145f736e79
wwagner4 <wwagner4@gmail.com>
2018-04-13 07:44:48 +0200

    finished grouping refactoring

--- vsoc-ga-2018/analyse/src/main/scala/vsoc/ga/analyse/group/Grouping.scala
+++ vsoc-ga-2018/analyse/src/main/scala/vsoc/ga/analyse/group/Grouping.scala
@@ -6,26 +6,26 @@ object Grouping {
 
   def group(in: Seq[Viz.XY], grpSize: Int): Seq[Viz.XY] = {
 
-    def doGroup(in: Seq[Viz.XY]): Seq[Viz.XY] = {
-      val head = in.head
-      val tail = in.tail
-      head :: doGroup1(tail)
+    def grp(inGrp: Seq[Viz.XY]): Seq[Viz.XY] = {
+      val head = inGrp.head
+      val tail = inGrp.tail
+      head :: grpTail(tail)
     }
 
-    def doGroup1(in: Seq[Viz.XY]): List[Viz.XY] = {
-      val grps: List[Seq[Viz.XY]] = in.grouped(grpSize).toList
-      grps.map(grp => doGroup2(grp))
+    def grpTail(inTail: Seq[Viz.XY]): List[Viz.XY] = {
+      val grps = inTail.grouped(grpSize)
+      grps.toList.map(means)
     }
 
-    def doGroup2(in: Seq[Viz.XY]): Viz.XY = {
-      require(in.nonEmpty)
-      val xs = in.map(xy => xy.x.doubleValue()).sum
-      val ys = in.map(xy => xy.y.doubleValue()).sum
-      Viz.XY(xs / in.size, ys / in.size)
+    def means(inGrp: Seq[Viz.XY]): Viz.XY = {
+      require(inGrp.nonEmpty)
+      val xs = inGrp.map(xy => xy.x.doubleValue()).sum
+      val ys = inGrp.map(xy => xy.y.doubleValue()).sum
+      Viz.XY(xs / inGrp.size, ys / inGrp.size)
     }
 
     if (in.size < 3 || grpSize == 1) in
-    else doGroup(in)
+    else grp(in)
   }
 
 }
