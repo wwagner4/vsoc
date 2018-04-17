0d8ee4ff1b7762af56416209b3600098871b9ac8
wwagner4 <wwagner4@gmail.com>
2018-04-17 12:49:04 +0200

    input data analyse

---
+++ vsoc-ga-2018/analyse/src/main/scala/vsoc/ga/analyse/Data01DiaMain.scala
@@ -0,0 +1,9 @@
+package vsoc.ga.analyse
+
+import vsoc.ga.common.config.Configs
+
+object Data01DiaMain extends App {
+
+  Data01Dia.createDiaConfig(Configs.work010)
+
+}
---
+++ vsoc-ga-2018/trainga/src/main/scala/vsoc/ga/trainga/nn/analyse/BehaviourInputDataAnalyse.scala
@@ -0,0 +1,26 @@
+package vsoc.ga.trainga.nn.analyse
+
+import java.util.Optional
+
+import atan.model.Player
+import vsoc.behaviour.{Behaviour, Sensors}
+import vsoc.ga.trainga.behav.InputMapperNn
+import vsoc.ga.trainga.nn.NeuralNet
+
+class BehaviourInputDataAnalyse(
+                          val neuralNet: NeuralNet,
+                          val child: Behaviour,
+                          val inputMapper: InputMapperNn) extends Behaviour {
+
+  override def shouldBeApplied(sens: Sensors): Boolean = {
+    val in: Option[Array[Double]] = inputMapper.mapSensors(sens)
+    in.foreach(a => println(a.toList))
+    false
+  }
+
+  override def apply(sens: Sensors, player: Player): Unit = {
+    throw new IllegalStateException("should never be called")
+  }
+
+  override def getChild: Optional[Behaviour] = Optional.of(child)
+}
