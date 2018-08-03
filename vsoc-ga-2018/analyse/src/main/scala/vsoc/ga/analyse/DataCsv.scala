65e3b91cccccf383ef7d75c7d7539c702b0cb805
wwagner4 <wwagner4@gmail.com>
2018-08-03 15:54:37 +0200

    fixed csv

--- vsoc-ga-2018/analyse/src/main/scala/vsoc/ga/analyse/DataCsv.scala
+++ vsoc-ga-2018/analyse/src/main/scala/vsoc/ga/analyse/DataCsv.scala
@@ -7,12 +7,41 @@ import vsoc.ga.common.config.{Config, ConfigHelper, ConfigTrainGa}
 
 abstract class DataCsv[T](csvReader: CsvReader[T]) {
 
-  private val _workDir = ConfigHelper.workDir
+  val sepa = "\t"
+
+  def fmtStr(fmtDef: Seq[Fmt]): String = {
+    fmtDef.map(_.toString).mkString(sepa)
+  }
+
+  def fmtHeader(headers: Seq[String]): String = {
+    headers.map(_.toString).mkString(sepa)
+  }
+
+  trait Formatter[T] {
+
+    def header: (PrintWriter) => Unit
+
+    def data: (T, PrintWriter) => Unit
+  }
+
+  trait Fmt
+  case object F extends Fmt {
+    override def toString: String = "%10.2f"
+  }
+  case object S extends Fmt {
+    override def toString: String = "%10s"
+  }
+
+  case object D extends Fmt {
+    override def toString: String = "%10d"
+  }
+
+  protected val _workDir = ConfigHelper.workDir
 
   def createCsvConfig(
                        cfg: Config,
                      ): Unit = {
-    val pw = createPrintWriter
+    val pw = createPrintWriter(cfg)
     try {
       createCsv(cfg.trainings, cfg.id, pw)
     } finally {
@@ -20,14 +49,15 @@ abstract class DataCsv[T](csvReader: CsvReader[T]) {
     }
   }
 
-  def createPrintWriter: PrintWriter = new PrintWriter(System.out)
+  def createPrintWriter(config: Config): PrintWriter = new PrintWriter(System.out)
 
-  def writeLine(line: T): Unit
+  def formatter: Formatter[T]
 
   private def createCsv(
                          trainGas: Seq[ConfigTrainGa],
                          diaId: String,
                          printWriter: PrintWriter): Unit = {
+    formatter.header(printWriter)
     for (ConfigTrainGa(id, nr) <- trainGas) {
       println(s"writing $id $nr")
       def prjDir = _workDir.resolve(Paths.get(id, nr))
@@ -38,7 +68,7 @@ abstract class DataCsv[T](csvReader: CsvReader[T]) {
 
       val n = raw.size
       println(s"found $n lines")
-      raw.foreach(l => writeLine(l))
+      raw.foreach(l => formatter.data(l, printWriter))
     }
 
 
--- vsoc-ga-2018/common/src/main/scala/vsoc/ga/common/data/Data.scala
+++ vsoc-ga-2018/common/src/main/scala/vsoc/ga/common/data/Data.scala
@@ -11,18 +11,22 @@ case class Data02(
                    trainGaId: String = "",
                    trainGaNr: String = "",
                    iterations: Int = 0,
+
                    kicksMax: Double = 0.0,
                    kicksMean: Double = 0.0,
-                   kickOutMin: Double = 0.0,
+                   kicksMin: Double = 0.0,
                    kickOutMax: Double = 0.0,
                    kickOutMean: Double = 0.0,
-                   kicksMin: Double = 0.0,
+                   kickOutMin: Double = 0.0,
+
                    otherGoalsMax: Double = 0.0,
                    otherGoalsMean: Double = 0.0,
                    otherGoalsMin: Double = 0.0,
                    ownGoalsMax: Double = 0.0,
                    ownGoalsMean: Double = 0.0,
                    ownGoalsMin: Double = 0.0,
+
                    goalDifference: Double = 0.0,
+
                    score: Double = 0.0,
                  )
