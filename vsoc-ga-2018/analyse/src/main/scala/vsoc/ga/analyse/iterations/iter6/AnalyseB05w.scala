45c39bebead1fcc5870a74edd8d5fb3e8ed12c5b
wwagner4 <wwagner4@gmail.com>
2018-11-05 17:51:26 +0100

    dias for iter6

---
+++ vsoc-ga-2018/analyse/src/main/scala/vsoc/ga/analyse/iterations/iter6/AnalyseB05w.scala
@@ -0,0 +1,11 @@
+package vsoc.ga.analyse.iterations.iter6
+
+import vsoc.ga.analyse.dia.CreateDiasData02
+import vsoc.ga.common.UtilPath
+
+object AnalyseB05w extends App {
+
+  val workDir = UtilPath.workDir
+  CreateDiasData02.create(DiaFactoriesB05w)(workDir)
+
+}
