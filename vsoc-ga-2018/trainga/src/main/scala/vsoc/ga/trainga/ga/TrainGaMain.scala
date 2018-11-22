13c2a4067ed9875d180e329a84cf0dddb66682e1
wwagner4 <wwagner4@gmail.com>
2018-11-22 17:48:08 +0100

    reverted main

--- vsoc-ga-2018/trainga/src/main/scala/vsoc/ga/trainga/ga/TrainGaMain.scala
+++ vsoc-ga-2018/trainga/src/main/scala/vsoc/ga/trainga/ga/TrainGaMain.scala
@@ -1,12 +1,18 @@
 package vsoc.ga.trainga.ga
 
 import vsoc.ga.common.commandline.WithConfigRunner
-import vsoc.ga.common.config.Configs
+import vsoc.ga.common.config.Config
 
 object TrainGaMain extends  WithConfigRunner {
 
   def main(args: Array[String]): Unit = {
-      new ConfigRunner().run(Configs.b05Test)
+    runWithConfig(args, trainGa, TrainGaMain.getClass.getSimpleName)
+
+    def trainGa(cfg: Config): Unit = {
+      new ConfigRunner().run(cfg)
   }
+  }
+
+
 
 }
