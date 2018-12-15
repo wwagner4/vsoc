4ddabc1adfdc6b92e4dea2eba75fc0e1afa34822
wwagner4 <wwagner4@gmail.com>
2018-12-15 10:52:32 +0100

    transforming to neural nets works fine

--- vsoc-ga-2018/trainga/src/main/scala/vsoc/ga/trainga/ga/impl/player01/TransformerPlayer01.scala
+++ vsoc-ga-2018/trainga/src/main/scala/vsoc/ga/trainga/ga/impl/player01/TransformerPlayer01.scala
@@ -2,12 +2,20 @@ package vsoc.ga.trainga.ga.impl.player01
 
 import vsoc.behaviour.Behaviour
 import vsoc.ga.genetic.Transformer
+import vsoc.ga.trainga.behav.{InputMappers, OutputMappers}
+import vsoc.ga.trainga.nn.NeuralNets
 
 class TransformerPlayer01 extends Transformer[Double, PhenoPlayer01] {
 
   override def toPheno(geno: Seq[Double]): PhenoPlayer01 = {
 
-    def behav: Behaviour = ???
+    def behav: Behaviour = {
+      val out = OutputMappers.om02
+      val in = InputMappers.default
+      val child = java.util.Optional.of(vsoc.ga.matches.Behaviours.remainOnField)
+      val net = NeuralNets.team02
+      new vsoc.ga.trainga.behav.BehaviourNeuralNet(net, child, in, out)
+    }
 
     PhenoPlayer01(geno, behav)
   }
