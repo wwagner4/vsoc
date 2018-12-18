94a6bdc7be3a7727fed71f134bc171c52013c7e6
wwagner4 <wwagner4@gmail.com>
2018-12-18 09:04:00 +0100

    chnage fitness function

--- vsoc-ga-2018/trainga/src/main/scala/vsoc/ga/trainga/ga/impl/player01/FitnessFunctionPlayer01.scala
+++ vsoc-ga-2018/trainga/src/main/scala/vsoc/ga/trainga/ga/impl/player01/FitnessFunctionPlayer01.scala
@@ -6,8 +6,6 @@ class FitnessFunctionPlayer01 extends FitnessFunction[DataPlayer01] {
   override def fitness(score: DataPlayer01): Double = {
     val k: Double = score.kicks
     val g: Double = score.goals
-    val s = k + g
-    println(s"-- fitness $k + $g = $s")
-    s
+    k + (g * 2.0)
   }
 }
